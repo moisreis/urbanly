cf919138d05b6ad682c50ba25073ba8a537840b2
Moisés Reis <jacobina.moreira.reis@gmail.com>
2025-06-05 13:52:56 -0300

    change: add the favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Urbanly",
+  title: "Urbanly. Sua cidade ao seu alcance",
   description: "Sua cidade ao seu alcance",
 };
 
