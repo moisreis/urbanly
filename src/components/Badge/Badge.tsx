c8b43bb6a05b51d6e0b15d1fdd5940374fd8c0c0
Moisés Reis <jacobina.moreira.reis@gmail.com>
2025-06-13 08:04:45 -0300

    improve: sets on mod, box, lyt for css convention and uses mixins in badge and add comments

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,24 +1,26 @@
 import clsx from "clsx";
-import scss from "./Badge.module.scss"; // Imports scoped styles for the component (modular SCSS)
+import styles from "./Badge.module.scss"; // Imports scoped styles for the component (modular SCSS)
 import utils from "./utils.module.scss";
 
 interface BadgeProps {
     label?: string;
-    variant: "primary" | "secondary" | "link" | "outline" | "blur";
-    hasBackground: boolean;
+    variant: "primary" | "secondary" | "tertiary" | "outline" | "blur";
+    hasBackground?: boolean;
     icon?: React.ReactNode;
 }
 
-const Badge: React.FC<BadgeProps> = ({label, variant, icon, hasBackground}) => {
-    // const variantClass = scss[`container_${variant}`];
-    // const backgroundClass = hasBackground ? scss.hasBackground : "";
+const Badge: React.FC<BadgeProps> = ({label, variant, icon, hasBackground = false}) => {
 
     return (
-        <div className={clsx(scss.container, scss[`container_${variant}`], {
-            [utils.hasBackground]: hasBackground
-        })}>
+        <div role="text" aria-label={label} className={
+            clsx(
+                styles.mod, styles[`mod_${variant}`], {
+                    [utils.hasBackground]: hasBackground
+                }
+            )
+        }>
+            {icon && (<div className={styles.mod_icon}>{icon}</div>)}
             {label && (<span>{label}</span>)}
-            {icon}
         </div>
     );
 };
